e2ceb0f43d221e2493bcd480e4bf9faff9daed04
ShayneWinn <shaynedwinn@gmail.com>
2019-03-24 12:39:53 -0400

    Started Question 12, Not finished.

---
+++ Question12/Question12/Question12.cpp
@@ -0,0 +1,21 @@
+// Question12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::cout << "Project Euler Question 12\n\n";
+
+
+}
+
+//Returns Factors of n
+std::vector<int> Factors(int _n)
+{
+	std::vector <int> _factors;
+
+	return _factors;
+}
